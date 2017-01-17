f17e2d75ef926f195cce957b89df8836b5b9f692
paul <pkwdigital@gmail.com>
2017-01-17 13:52:52 -0500

    Add search form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
+
 def show
-	parameters = { term: params[:search], limit: 20}
+	parameters = {term: params[:search], limit: 20}
 	@response = Yelp.client.search('New York', parameters)
 	end
 
