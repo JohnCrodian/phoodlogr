e8e7c2c97e1127390bcd79befb55e92255f0d50b
paul <pkwdigital@gmail.com>
2017-01-19 16:15:45 -0500

    fix conflicts

--- db/migrate/20170119150856_add_yelp_ids_and_users_to_add_favorite.rb
+++
@@ -1,6 +0,0 @@
-class AddYelpIdsAndUsersToAddFavorite < ActiveRecord::Migration[5.0]
-  def change
-    add_column :add_favorites, :yelp_ids, :string
-    add_column :add_favorites, :user_id, :integer
-  end
-end
