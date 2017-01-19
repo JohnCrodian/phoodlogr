ed08c3bdcd5db1702236a9ad9c7791982ecaa027
Ryan Courtney <ryan.courtney1@gmail.com>
2017-01-19 14:35:38 -0500

    made search bar, created profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  get 'profiles/show'
+
+  get 'profiles/index'
+
+  get 'profiles/create'
+
+  get 'profiles/edit'
+
+  get 'profiles/destroy'
+
+  get 'profiles/update'
+
+  get 'profiles/all'
+
+  get 'profiles/new'
+
   devise_for :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
